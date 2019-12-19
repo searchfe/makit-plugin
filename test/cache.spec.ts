31492ee3c7adb72a71cf2315f2bb9b74483ecb70
qiansc <van3008@163.com>
2019-12-19 11:34:37 +0800

    feat: add test

---
+++ test/cache.spec.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: qiansc
+ * @Date: 2019-04-24 15:54:24
+ * @Last Modified by: qiansc
+ * @Last Modified time: 2019-12-19 11:34:24
+ */
+
+
+describe('Test', () => {
+    it('nothing', () => {
+    });
+});
